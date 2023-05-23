6154532cd4e3ec2b62f0c58fffff50ba3f5524e7
Dmitry <dmitry.guslov-gans@yandex.ru>
2023-05-23 22:44:18 +0300

    Решил 14 задачу

---
+++ HomeWorkPython/Task14.py
@@ -0,0 +1,15 @@
+# Задача 14:
+# Требуется вывести все целые степени двойки (т.е. числа вида 2k),
+# не превосходящие числа N.
+# 10 -> 1 2 4 8
+
+number = int(input("Введите степень для 2: "))
+result = " "
+
+for i in range(number):
+    if 2** i < number:
+        tmp = 2**i
+        result = result + str(tmp) + ", "
+        i += 1
+print(f"{number} -> {result}")
+
