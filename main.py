2d881db272b0f1e97ab773537d53748259f9541c
Dmitry <dmitry.guslov-gans@yandex.ru>
2023-06-02 15:36:36 +0300

    Решил 16 задачу

--- main.py
+++ main.py
@@ -9,9 +9,26 @@ elif username == 'Ильнар':
 else:
     print('Привет,', username)
 """
-line = ""
-for i in range(5):
-    line = ""
-    for j in range(5):
-        line += "*"
-    print(line)
+# line = ""
+# for i in range(5):
+#     line = ""
+#     for j in range(5):
+#         line += "*"
+#     print(line)
+
+# Пробуем импортировать модуль
+
+import modul1
+modul1.max1(5, 9)
+print(modul1.max1(5, 9))
+
+# Вызываем сразу функцию
+
+from modul1 import max1 # Можно вместо max1 написать * - это значит, что импортируются все функции из файла
+print(max1(5, 9))
+
+# Можно так же переименовать файл в самой программе, но не сам файл,
+# тем самым упростить ввод в будущем:
+
+import modul1 as m1 # С помощью as задаем новое имя 
+print(m1.max1(5, 9))
