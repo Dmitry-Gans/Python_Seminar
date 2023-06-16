dc20af0edee17bb8f84607205f81c1f18651963f
Dmitry <dmitry.guslov-gans@yandex.ru>
2023-06-16 20:24:39 +0300

    Решил 51 задачу

--- Seminar7/Task51.py
+++ Seminar7/Task51.py
@@ -19,6 +19,17 @@
 # same
 
 values = [0, 2, 10, 6] 
+
+# def same_by(function, objects):
+#     if len(objects) == 0:
+#         print ('Список пустой')
+#     else:
+#         list_1 = list(map(function, objects))
+#         return max(list_1) == min(list_1)
+
+def same_by(function, objects):
+    return len(list(filter(function, objects))) == 0
+
 if same_by(lambda x: x % 2, values):
     print('same')
 else:
